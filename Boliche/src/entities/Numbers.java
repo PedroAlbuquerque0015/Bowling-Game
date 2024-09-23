3caa56d39d82e14b52ee909986c6809ccb679299
PedroAlbuquerque0015 <pedroalbuquerque361@gmail.com>
2024-09-22 22:32:33 -0300

    Update Numbers to english.java

--- Boliche/src/entities/Numbers.java
+++ Boliche/src/entities/Numbers.java
@@ -10,15 +10,15 @@ public class Numbers {
 	
 		switch (pins) {
 		case 10: System.out.println("GUTTER!");break;
-		case 9: System.out.println("Faltam 9 pinos"); break;
-		case 8: System.out.println("Faltam 8 pinos"); break;
-		case 7: System.out.println("Faltam 7 pinos"); break;
-		case 6: System.out.println("Faltam 6 pinos"); break;
-		case 5: System.out.println("Faltam 5 pinos"); break;
-		case 4: System.out.println("Faltam 4 pinos"); break;
-		case 3: System.out.println("Faltam 3 pinos"); break;
-		case 2: System.out.println("Faltam 2 pinos"); break;
-		case 1: System.out.println("Faltam 1 pinos");break;
+		case 9: System.out.println("9 pins left"); break;
+		case 8: System.out.println("8 pins left"); break;
+		case 7: System.out.println("7 pins left"); break;
+		case 6: System.out.println("6 pins left"); break;
+		case 5: System.out.println("5 pins left"); break;
+		case 4: System.out.println("4 pins left"); break;
+		case 3: System.out.println("3 pins left"); break;
+		case 2: System.out.println("2 pins left"); break;
+		case 1: System.out.println("1 pin left");break;
 		}
 		
 	}
